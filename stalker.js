0762f79b33fe46c1caddcddbd18556ac1510b9b9
jonnykernal <jonathan.jackson@student.msdmartinsville.org>
2026-08-20 18:07:32 -0400

    Update stalker.js

--- stalker.js
+++ stalker.js
@@ -1,7 +1,17 @@
 ModAPI.require("player");
 
+var playerTested = false;
+
 ModAPI.addEventListener("update", function () {
+    if (playerTested) {
+        return;
+    }
+
+    playerTested = true;
+
     if (ModAPI.player) {
-        ModAPI.displayToChat("§a[STALKER] Player API works!");
+        ModAPI.displayToChat("§a[STALKER] Player access works!");
+    } else {
+        ModAPI.displayToChat("§c[STALKER] Player access returned nothing.");
     }
 });
